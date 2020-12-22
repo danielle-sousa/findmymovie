df3bf4de94593be7e272e9fd1c08c7f5961e3654
Danielle <daniellexsousa@gmail.com>
2020-12-21 21:43:38 -0300

    css update

--- findmymovie/src/app/app.module.ts
+++ findmymovie/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RodapeComponent } from './shared/components/rodape/rodape.component';
 import { AlertaComponent } from './shared/components/alerta/alerta.component';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
